ae8df627d1af2c34edb983a65150b4f837dcc7ef
User <kenobi88@gmail.com>
2025-10-21 16:13:13 +0300

    1-commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Happy developing ✨')
+
+function factorialIterative(n) {
+    if (n < 0) {
+        return 'Foo! No factorial for negative numbers!';
+    }
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+console.log(factorialIterative(5));
